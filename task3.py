128b79cf493389fdf1b62ff9fb87e6d26847b993
Evgenii <kachaloff89@gmail.com>
2023-01-12 23:36:58 +0300

    Add files

---
+++ task3.py
@@ -0,0 +1,11 @@
+# 3.Узнайте у пользователя число n.
+
+n = int(input('Введите число: '))
+
+# Найдите сумму чисел n + nn + nnn.
+
+str_n = str(n)
+n2 = str_n + str_n
+n3 = str_n + str_n + str_n
+total = n + int(n2) + int(n3)
+print("Сумма всех чисел:", total)
